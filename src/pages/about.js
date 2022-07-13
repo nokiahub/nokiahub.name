88cce3049e57382a2b451f2fa90d4bdf930a0ce6
nokiahub <jeonghj@in8.kr>
2022-07-13 16:44:27 +0900

    about 업데이트

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,11 @@ const About = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <Seo title={siteTitle} description={siteTitle} />
       <p>
-        안녕하세요. 프론트엔드 개발자 정형주입니다.
+        안녕하세요. 1년차 프론트엔드 개발자 정형주입니다.
+        <br />
+        알고리즘, 클린코드, 리펙토링에 관심이 있습니다.
+        <br />
+        유연하고 확장 가능한 프로그램 설계를 좋아합니다.
         <br />
         자세한 내용은{' '}
         <a href="https://drive.google.com/file/d/1_s8wR2kTMbsI0mSXoKcD9K9M40wwccgb/view?usp=sharing">
