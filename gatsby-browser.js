1d7b2962f4c4d8c9966fedd099b011b996d97b42
nokiahub <jeonghj157@gmail.com>
2022-08-23 02:35:30 +0900

    StyledThemeProvider, GlobalStyle 레이아웃으로 이동

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,21 +6,14 @@ import 'typeface-merriweather';
 import 'prismjs/themes/prism-okaidia.css';
 
 import React from 'react';
-import { graphql } from 'gatsby';
 
 import { ThemeProvider } from './src/contexts/themeContext';
-import StyledThemeProvider from './src/styles/StyledThemeProvider';
-import GlobalStyle from './src/styles/GlobalStyle';
-
 import Layout from './src/components/Layout';
 
 export const wrapRootElement = ({ element }) => {
   return (
     <ThemeProvider>
-      <StyledThemeProvider>
-        <GlobalStyle />
         {element}
-      </StyledThemeProvider>
     </ThemeProvider>
   );
 };
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { graphql } from 'gatsby';
 
 import { ThemeProvider } from './src/contexts/themeContext';
-import StyledThemeProvider from './src/styles/StyledThemeProvider';
-import GlobalStyle from './src/styles/GlobalStyle';
-
 import Layout from './src/components/Layout';
 
 export const wrapRootElement = ({ element }) => {
   return (
     <ThemeProvider>
-      <StyledThemeProvider>
-        <GlobalStyle />
         {element}
-      </StyledThemeProvider>
     </ThemeProvider>
   );
 };
