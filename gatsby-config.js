773991ff9c614812c7c334c9880b9de98a25d509
nokiahub <hjjeong@ktown4u.com>
2023-10-05 18:31:16 +0900

    google font 관련 설정 및 모듈 삭제

--- gatsby-config.js
+++ gatsby-config.js
@@ -145,16 +145,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `NanumGothic`,
-          `source sans pro\:400,700,900` // you can also specify font weights and styles
-        ],
-        display: 'swap'
-      }
-    },
     `gatsby-plugin-styled-components`
   ]
 };
