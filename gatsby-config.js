ef20c5931a59d2f0261c450e756efbf9bbacf51e
nokiahub <hjjeong@ktown4u.com>
2023-10-15 03:05:08 +0900

    절대경로 설정

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-root-import`,
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
