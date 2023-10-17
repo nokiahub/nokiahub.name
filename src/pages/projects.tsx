816d4ff958433bc17af7768c3b66288291892f26
nokiahub <hjjeong@ktown4u.com>
2023-10-17 09:16:14 +0900

    프로젝트 목록 화면 추가

---
+++ src/pages/projects.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import { Link, graphql } from 'gatsby';
+import styled from 'styled-components';
+
+import Seo from 'src/components/seo';
+import { spacing, typography } from 'src/constants/styles';
+
+const PostListItem = styled.article`
+  margin-bottom: ${spacing.spacing8};
+  margin-top: ${spacing.spacing8};
+`;
+
+const PostHeader = styled.header`
+  margin-bottom: ${spacing.spacing4};
+`;
+
+const PostTitle = styled.h1`
+  font-size: ${typography.fontSize5};
+  margin-bottom: ${spacing.spacing2};
+  margin-top: ${spacing.spacing0};
+  transition: ease-in-out 0.1s;
+`;
+
+const PostTitleLink = styled(Link)`
+  color: ${(props) => props.theme.primary};
+`;
+
+const ProjectsTemplate = ({ data }) => {
+  const projects = data.allMarkdownRemark.nodes;
+
+  return (
+    <>
+      <Seo title="형주의 블로그" />
+      <ol style={{ listStyle: `none` }}>
+        {projects.map((item) => {
+          const title = item.frontmatter.title || item.fields.slug;
+
+          return (
+            <li key={item.fields.slug}>
+              <PostListItem itemScope itemType="http://schema.org/Article">
+                <PostHeader>
+                  <PostTitle>
+                    <PostTitleLink to={item.fields.slug} itemProp="url">
+                      {title}
+                    </PostTitleLink>
+                  </PostTitle>
+                </PostHeader>
+              </PostListItem>
+            </li>
+          );
+        })}
+      </ol>
+    </>
+  );
+};
+
+export default ProjectsTemplate;
+
+export const pageQuery = graphql`
+  query blogListQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      filter: {
+        frontmatter: {
+          status: { ne: "draft" }
+          category: { eq: "project" }
+        }
+      },
+    ) {
+      nodes {
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+        }
+      }
+    }
+  }
+`;
