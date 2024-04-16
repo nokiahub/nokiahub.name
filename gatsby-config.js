849edb2f4354fb0377cdb692cdd93484ca46bfd9
nokiahub <hjjeong@ktown4u.com>
2024-04-16 12:28:34 +0900

    updated package versions, added tailwind package

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,12 +58,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
@@ -143,9 +137,7 @@ module.exports = {
       }
     },
     `gatsby-plugin-react-helmet`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    'gatsby-plugin-postcss',
     `gatsby-plugin-styled-components`
   ]
 };
