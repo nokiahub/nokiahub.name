0ba5e022af09920c2a4f8759f954e75fc5210139
nokiahub <hjjeong@ktown4u.com>
2024-05-07 17:48:52 +0900

    added post page

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,3 +1,17 @@
-export default function Page({ params }: { params: { id: string } }) {
-  return <div>My Post: {params.id}</div>;
+import { getAllPostIds, getPostData } from '../../../lib/post';
+
+export default async function Post({ params }: { params: { postId: string } }) {
+  const postData = await getPostData(params.postId);
+
+  return (
+    <div>
+      <div>{postData.title}</div>
+      <div>{postData.date}</div>
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </div>
+  );
+}
+
+export async function generateStaticParams() {
+  return getAllPostIds();
 }
--- lib/post.ts
+++ lib/post.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import { remark } from 'remark';
+import html from 'remark-html';
 
 const postsDirectory = path.join(process.cwd(), 'content/blog/dev');
 
@@ -28,4 +30,32 @@ export function getPostsData() {
       ...matterResult.data
     } as Post;
   });
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, '')
+      }
+    };
+  });
+}
+
+export async function getPostData(id: string) {
+  const fullPath = path.join(postsDirectory, `${id}`);
+  const filePath = path.join(fullPath, 'index.md');
+  const fileContents = fs.readFileSync(filePath, 'utf8');
+
+  const matterResult = matter(fileContents);
+
+  const processedContent = await remark().use(html).process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
+  return {
+    id,
+    contentHtml,
+    ...matterResult.data
+  } as Post & { contentHtml: string };
 }
