9e4919a97e0d3c6f1b8f3813dcf3f927eabc2430
nokiahub <hjjeong@ktown4u.com>
2024-05-09 16:52:59 +0900

    removed gatsby based project

--- .github/PULL_REQUEST_TEMPLATE.md
+++
@@ -1,4 +0,0 @@
-Click the `Preview` tab and select a PR template:
-
-- [Standard change](?expand=1&template=pull_request_template.md)
-- [Small change](?expand=1&template=bug_fix_template.md)
--- src/constants/styles/index.ts
+++
@@ -1,6 +0,0 @@
-import size from './size';
-import spacing from './spacing';
-import theme from './theme';
-import typography from './typography';
-
-export { size, spacing, theme, typography };
