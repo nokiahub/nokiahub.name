c0416e166456d3b72fb5f0bcf691f90d97f932be
nokiahub <hjjeong@ktown4u.com>
2024-05-09 17:30:58 +0900

    removed ts-ignore

--- app/components/providers.tsx
+++ app/components/providers.tsx
@@ -2,6 +2,5 @@ import { type ThemeProviderProps } from 'next-themes/dist/types';
 import { ThemeProvider } from 'next-themes';
 
 export function Providers({ children }: ThemeProviderProps) {
-  // @ts-ignore
   return <ThemeProvider>{children}</ThemeProvider>;
 }
