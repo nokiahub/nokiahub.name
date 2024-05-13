c25a4caab5a814f8c5e365f5bc58196d660f6d4e
nokiahub <hjjeong@ktown4u.com>
2024-05-13 13:56:31 +0900

    fixed css to inline

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,23 +5,23 @@ import { AboutIcon } from '../icons/AboutIcon';
 
 export const Header = () => {
   return (
-    <nav className={wrapperStyle}>
-      <div>
+    <nav className={'bg-inherit  flex justify-between items-center fixed top-0 left-0 w-full border-b p-[1.25rem] pt-[0.75rem] pb-[1rem]'}>
+      <div className={"bg-opacity-10"}>
         <Link href={'/'}>형주의 블로그</Link>
       </div>
       <div className={'flex gap-[1.5rem]'}>
-        <div className={linkItemStyle}>
+        <div className={'flex flex-col justify-center text-lg m-0'}>
           <Link href={'/projects'}>Projects</Link>
         </div>
-        <div className={linkItemStyle}>
+        <div className={'flex flex-col justify-center text-lg m-0'}>
           <ThemeSwitchToggle />
         </div>
-        <div className={linkItemStyle}>
+        <div className={'flex flex-col justify-center text-lg m-0'}>
           <Link target="_blank" href="https://github.com/nokiahub" aria-label="my github account">
             <GithubIcon />
           </Link>
         </div>
-        <div className={linkItemStyle}>
+        <div className={'flex flex-col justify-center text-lg m-0'}>
           <Link href="/about" aria-label="about me">
             <AboutIcon />
           </Link>
@@ -31,7 +31,3 @@ export const Header = () => {
   );
 };
 
-const wrapperStyle =
-  'flex justify-between items-center fixed top-0 left-0 cursor-pointer z-1000 w-full border-b bg-background text-2xl p-[1.25rem] pt-[0.75rem] pb-[1rem]';
-
-const linkItemStyle = 'flex flex-col justify-center text-lg m-0';
