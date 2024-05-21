5458541cc9c5e62582d5d240cd454751a451e72b
nokiahub <hjjeong@ktown4u.com>
2024-05-21 14:33:57 +0900

    removed project page

--- app/projects/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getProjectsData } from '@/lib/post';
-import { Projects } from '../components/Projects';
-
-export default function Page() {
-  const projects = getProjectsData();
-  return <Projects items={projects} />;
-}
