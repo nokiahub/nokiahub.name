2f532109549a1bc95d5674131078422ccc15bf20
nokiahub <hjjeong@ktown4u.com>
2024-06-19 15:28:54 +0900

    fixed project page css on mobile view

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,3 @@
-import { Card, CardContent } from "@/components/ui/card";
 import { allProjects } from "contentlayer/generated";
 import MdxComponents from "@/components/mdx/mdx-components";
 import { Separator } from "@/components/ui/separator";
@@ -7,13 +6,11 @@ export default async function Projects() {
   return (
     <ul>
       {allProjects.map((project) => (
-        <li key={project.title} className={"mb-4"}>
-          <Card className="prose w-full max-w-[640px] border-none p-4 dark:prose-invert">
-            <CardContent>
-              <MdxComponents post={project} />
-            </CardContent>
-          </Card>
-          <Separator />
+        <li key={project.title}>
+          <div className="prose w-full max-w-[640px] border-none dark:prose-invert">
+            <MdxComponents post={project} />
+          </div>
+          <Separator className={"my-8"} />
         </li>
       ))}
     </ul>
