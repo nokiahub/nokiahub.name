d01c3c6d857dbb4e6ad236decddf29f8d3b196e0
nokiahub <hjjeong@ktown4u.com>
2024-08-05 10:58:18 +0900

    changed layout width

--- app/posts/[slug]/layout.tsx
+++ app/posts/[slug]/layout.tsx
@@ -2,5 +2,5 @@ import { ReactNode } from "react";
 import "@/styles/mdx.css";
 
 export default function ArticleLayout({ children }: { children: ReactNode }) {
-  return <div className={"max-w-[40rem]"}>{children}</div>;
+  return <div className={"max-w-[48rem]"}>{children}</div>;
 }
