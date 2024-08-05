4d34422aab468e7db6d37d6bc5ab52e575cc81b2
nokiahub <hjjeong@ktown4u.com>
2024-08-05 17:34:15 +0900

    added about image

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,38 +1,51 @@
 import { Metadata } from "next";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "I'm Hyungju.",
 };
 
 export default function Page() {
-  const startDay = new Date("2021-07");
-  const today = new Date();
-  const diff = today.getTime() - startDay.getTime();
-  const diffYear = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
   return (
-    <>
-      <p>
-        안녕하세요. {diffYear}년차 프론트엔드 개발자 정형주입니다.
-        <br />
-        알고리즘, 클린코드, 리팩토링에 관심이 있습니다.
-        <br />
-        유연하고 확장 가능한 프로그램 설계를 좋아합니다.
-        <br />
-      </p>
+    <div className={cn("grid grid-cols-1  gap-8 sm:grid-cols-2")}>
+      <div className={cn("row-span-3")}>
+        <p>
+          안녕하세요. 3년차 프론트엔드 개발자 정형주입니다.
+          <br />
+          알고리즘, 클린코드, 리팩토링에 관심이 있습니다.
+          <br />
+          유연하고 확장 가능한 프로그램 설계를 좋아합니다.
+          <br />
+        </p>
 
-      <p>
-        <br />더 자세한 내용은{" "}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://hazel-case-679.notion.site/29-Frontend-Developer-80d76411929b4b9e9d1cc034177385b7?pvs=4"
-        >
-          이력서
-        </a>
-        에서 확인하실 수 있습니다.
-        <br />
-        감사합니다!
-      </p>
-    </>
+        <div className={cn("row-span-2")}>
+          <p>
+            <br />더 자세한 내용은{" "}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://hazel-case-679.notion.site/29-Frontend-Developer-80d76411929b4b9e9d1cc034177385b7?pvs=4"
+            >
+              이력서
+            </a>
+            에서 확인하실 수 있습니다.
+            <br />
+            감사합니다!
+          </p>
+        </div>
+      </div>
+      <div>
+        <div className={cn("row-span-5")}>
+          <Image
+            className={"rotate-3 rounded-xl"}
+            src={"/images/me.jpeg"}
+            alt={"profile picture"}
+            width={300}
+            height={400}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
