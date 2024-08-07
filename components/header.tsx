d8ded4e5f568eed7e3049067a2991eab02f01cc4
nokiahub <hjjeong@ktown4u.com>
2024-08-07 09:54:38 +0900

    fixed scroll top button to not show when sidenav is opened

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
   return (
     <div
       className={
-        "sticky top-0 flex items-center justify-between border-b border-border/45 bg-background px-6 py-3"
+        "sticky top-0 z-10 flex items-center justify-between border-b border-border/45 bg-background px-6 py-3"
       }
     >
       <Link className={"text-end text-xl"} href={"/"}>
