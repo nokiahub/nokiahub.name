ed19fa78af8b5867a2564957a7d203ef5a9dd406
nokiahub <hjjeong@ktown4u.com>
2024-08-07 10:27:07 +0900

    fixed right nav

--- components/right-nav.tsx
+++ components/right-nav.tsx
@@ -1,31 +1,46 @@
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { ArrowRight } from "lucide-react";
+import ThemeSwitchToggle from "@/components/theme-switch-toggle";
+import { MouseEvent } from "react";
 
 type Props = {
   onClose: () => void;
 };
 
+const links = [
+  { href: "/posts", label: "Posts" },
+  { href: "/projects", label: "Projects" },
+  { href: "/about", label: "Info" },
+];
+
 export default function RightNav({ onClose }: Props) {
+  const handleInnerClick = (e: MouseEvent) => {
+    e.stopPropagation();
+  };
+
   return (
-    <div className={cn("fixed inset-0 backdrop-brightness-50")}>
+    <div
+      className={cn("fixed inset-0 backdrop-brightness-50")}
+      onClick={onClose}
+    >
       <div
         className={cn("fixed inset-y-0 right-0 w-[75%] bg-background p-4 px-6")}
+        onClick={handleInnerClick}
       >
         <button onClick={onClose}>
           <ArrowRight />
         </button>
         <ul>
-          <li>
-            <Link href={"/posts"}>Posts</Link>
-          </li>
-          <li>
-            <Link href={"/projects"}>Projects</Link>
-          </li>
-          <li>
-            <Link href={"/about"}>Info</Link>
-          </li>
+          {links.map(({ href, label }) => (
+            <li key={href}>
+              <Link href={href} onClick={onClose}>
+                {label}
+              </Link>
+            </li>
+          ))}
         </ul>
+        <ThemeSwitchToggle />
       </div>
     </div>
   );
