9e959323b25b2d7fd62b17fe41774226f23837f6
nokiahub <hjjeong@ktown4u.com>
2024-09-13 12:13:01 +0900

    added /posts/views route

---
+++ app/posts/views/route.ts
@@ -0,0 +1,19 @@
+// get all posts views from the database
+import prisma from "@/lib/prisma.server";
+
+export async function GET() {
+  const posts = await prisma.post.findMany({
+    select: {
+      slug: true,
+      views: true,
+    },
+  });
+
+  // return like this:
+  // { "slug": 0, "slug2": 0, "slug3": 0 ... }
+  const viewsBySlug = Object.fromEntries(
+    posts.map((post) => [post.slug, post.views]),
+  );
+
+  return new Response(JSON.stringify(viewsBySlug));
+}
