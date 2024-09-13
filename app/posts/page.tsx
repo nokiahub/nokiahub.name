a86bf8e65f0b6543647a43cd2f3d76b6201fb1b1
nokiahub <hjjeong@ktown4u.com>
2024-09-13 13:42:47 +0900

    fixed fetch views

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,18 +1,30 @@
 import { Posts } from "@/components/posts";
 import { Tags } from "@/components/tags";
 import { cn } from "@/lib/utils";
+import prisma from "@/lib/prisma.server";
 
 const getViews = async () => {
-  const response = await fetch(process.env.NEXT_PUBLIC_URL + "/posts/views", {
-    method: "GET",
+  const posts = await prisma.post.findMany({
+    select: {
+      slug: true,
+      views: true,
+    },
   });
 
-  return response.json();
+  // return like this:
+  // { "slug": 0, "slug2": 0, "slug3": 0 ... }
+  const viewsBySlug = Object.fromEntries(
+    posts.map((post: { slug: string; views: number }) => [
+      post.slug,
+      post.views,
+    ]),
+  );
+
+  return viewsBySlug;
 };
 
 const PostsPage = async () => {
   const views = await getViews();
-  console.log(views);
 
   return (
     <div className={cn("flex flex-col gap-6")}>
