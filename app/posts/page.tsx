6cb14e983ec2dc8644e8b050d93b7e90d0aec1f9
nokiahub <hjjeong@ktown4u.com>
2024-09-13 13:51:16 +0900

    inlined variables

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -13,14 +13,12 @@ const getViews = async () => {
 
   // return like this:
   // { "slug": 0, "slug2": 0, "slug3": 0 ... }
-  const viewsBySlug = Object.fromEntries(
+  return Object.fromEntries(
     posts.map((post: { slug: string; views: number }) => [
       post.slug,
       post.views,
     ]),
   );
-
-  return viewsBySlug;
 };
 
 const PostsPage = async () => {
