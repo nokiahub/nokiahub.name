cfe119bd1a8f57e8e3fbae973bd364bad556f989
nokiahub <hjjeong@ktown4u.com>
2025-01-15 12:01:38 +0900

    scroll down일 때 header가 보이지 않도록 스타일 추가

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,36 @@
+"use client";
+
 import Link from "next/link";
 import Nav from "@/components/nav";
 import Burger from "@/components/burger";
 import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
 
 export const Header = () => {
+  const [isHidden, setIsHidden] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+
+    setIsHidden(currentScrollY > lastScrollY);
+    setLastScrollY(currentScrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
     <div
-      className={
-        "sticky top-0 z-10 flex items-center justify-between border-b border-border/45 bg-background px-6 py-3"
-      }
+      className={cn(
+        "sticky top-0 z-10 flex items-center justify-between border-b border-border/45 bg-background px-6 py-3",
+        isHidden ? "invisible" : "",
+      )}
     >
       <Link className={"text-end text-xl"} href={"/"}>
         nokia&apos;s blog
